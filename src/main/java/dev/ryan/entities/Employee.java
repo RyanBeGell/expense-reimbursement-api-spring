31553a04252d8c6c7e3a8c49f12192f36ab94678
RyanBeGell <Ryanbegell@outlook.com>
2022-07-26 17:03:38 -0400

    Employee entity created

---
+++ src/main/java/dev/ryan/entities/Employee.java
@@ -0,0 +1,15 @@
+package dev.ryan.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private int employeeId;
+    private String firstName,lastName;
+}
