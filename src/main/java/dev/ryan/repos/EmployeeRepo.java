6ed9a9779139effe3234438e37c2776e5bf19d70
RyanBeGell <Ryanbegell@outlook.com>
2022-07-27 16:01:30 -0400

    Employee Crud Repo added

---
+++ src/main/java/dev/ryan/repos/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package dev.ryan.repos;
+
+import dev.ryan.entities.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends CrudRepository<Employee, String> { }
