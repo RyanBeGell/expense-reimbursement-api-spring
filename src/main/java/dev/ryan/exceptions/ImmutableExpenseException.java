5ed4cec7828ec9926566ac2f6c6b8e5747be8d21
RyanBeGell <Ryanbegell@outlook.com>
2022-08-01 13:55:02 -0400

    Exception message formatting tweak

--- src/main/java/dev/ryan/exceptions/ImmutableExpenseException.java
+++ src/main/java/dev/ryan/exceptions/ImmutableExpenseException.java
@@ -2,6 +2,6 @@ package dev.ryan.exceptions;
 
 public class ImmutableExpenseException extends RuntimeException{
     public ImmutableExpenseException(int id){
-        super("The expense with id #" + id + " has already been approved or denied and can not be changed.");
+        super("Expense #" + id + " has already been approved or denied and can not be changed.");
     }
 }
