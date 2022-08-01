22a43137ac970c355184e3dd4bec00751a6b65cd
RyanBeGell <Ryanbegell@outlook.com>
2022-08-01 11:58:52 -0400

    EmployeeServiceImpl added

---
+++ src/main/java/dev/ryan/services/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package dev.ryan.services;
+
+import dev.ryan.entities.Employee;
+import dev.ryan.repos.EmployeeRepo;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepo employeeRepo;
+
+    @Override
+    public Employee registerNewEmployee(Employee employee) {
+        return this.employeeRepo.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return this.employeeRepo.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(int id) {
+        Optional<Employee> possibleEmployee = this.employeeRepo.findById(id);
+
+        if(possibleEmployee.isPresent()){
+            return possibleEmployee.get();
+        } else{
+            throw new EntityNotFoundException("Employee with id #" + id +"not found.");
+        }
+    }
+
+    @Override
+    public Employee replaceEmployee(Employee employee) {
+       return employeeRepo.save(employee);
+    }
+
+    @Override
+    public boolean deleteEmployeeById(int id) {
+            // return true if deleted, otherwise exception would already be thrown if not found.
+            employeeRepo.delete(this.getEmployeeById(id));
+            return true;
+    }
+}
