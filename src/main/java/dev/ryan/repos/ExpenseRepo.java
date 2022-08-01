af14ad3413ed264ffe46887074fa4f1b223670d2
RyanBeGell <Ryanbegell@outlook.com>
2022-08-01 10:55:04 -0400

    CrudRepository changed to JpaRepository

--- src/main/java/dev/ryan/repos/ExpenseRepo.java
+++ src/main/java/dev/ryan/repos/ExpenseRepo.java
@@ -1,8 +1,10 @@
 package dev.ryan.repos;
 
 import dev.ryan.entities.Expense;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExpenseRepo extends CrudRepository <Expense,Integer> { }
+public interface ExpenseRepo extends JpaRepository<Expense,Integer> {
+
+}
