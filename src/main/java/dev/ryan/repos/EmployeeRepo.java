377bbb6fda2b8464819cfd624a938ea60317f097
RyanBeGell <Ryanbegell@outlook.com>
2022-08-01 11:58:29 -0400

    ID changed from String to Integer

--- src/main/java/dev/ryan/repos/EmployeeRepo.java
+++ src/main/java/dev/ryan/repos/EmployeeRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepo extends JpaRepository<Employee, String> {
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 
 }
