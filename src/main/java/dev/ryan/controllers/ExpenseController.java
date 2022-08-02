f07d1d88044512634d2c09770e474cc3ee97ac98
RyanBeGell <Ryanbegell@outlook.com>
2022-08-02 11:55:38 -0400

    Expense Controller added

---
+++ src/main/java/dev/ryan/controllers/ExpenseController.java
@@ -0,0 +1,65 @@
+package dev.ryan.controllers;
+
+import dev.ryan.entities.Expense;
+import dev.ryan.services.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @PostMapping("/expenses")
+    public Expense createEmployee(@RequestBody Expense expense){
+        return expenseService.registerNewExpense(expense);
+    }
+
+    @GetMapping("/expenses")
+    public List<Expense> getAllExpenses(@RequestParam String status) {
+        //If no query param, provide all expenses
+        if (status == null){
+            return expenseService.getAllExpenses();
+        } else {
+            //if status provided as query param, provide all expenses with that specific status
+            return expenseService.getAllExpensesByApprovalStatus(status);
+        }
+    }
+
+    @GetMapping("/employees/{id}/expenses")
+    public List<Expense> getExpensesByEmployeeId(@PathVariable int id){
+        return expenseService.getAllExpensesByEmployeeId(id);
+    }
+
+    @GetMapping("/expenses/{id}")
+    public Expense getExpenseById(@PathVariable int id){
+        return expenseService.retrieveExpenseByExpenseId(id);
+    }
+
+    @PutMapping("/expenses/{id}")
+    public Expense replaceExpenseById(@PathVariable int id){
+        return expenseService.replaceExpense(expenseService.retrieveExpenseByExpenseId(id));
+    }
+
+    @PatchMapping("/expenses/{id}/approve")
+    public String approveExpense(@PathVariable int id){
+        expenseService.approveExpense(expenseService.retrieveExpenseByExpenseId(id));
+        return("Expense #" + id + "has been approved.");
+    }
+
+    @PatchMapping("/expenses/{id}/deny")
+    public String denyExpense(@PathVariable int id){
+        expenseService.denyExpense(expenseService.retrieveExpenseByExpenseId(id));
+        return("Expense #" + id + "has been denied.");
+    }
+
+    @DeleteMapping("/expenses/{id}")
+    public String deleteExpense(@PathVariable int id){
+        expenseService.deleteExpenseById(id);
+        return ("Expense #" + id + "successfully deleted.");
+    }
+}
